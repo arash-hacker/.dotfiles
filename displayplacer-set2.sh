2d23be35af92057ef738b5501601e3c44428357b
arash-hacker <Arshiamidos@gmail.com>
2023-08-23 09:20:52 +0200

    Create displayplacer-set2.sh

---
+++ displayplacer-set2.sh
@@ -0,0 +1,1 @@
+displayplacer "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1728x1117 hz:120 color_depth:8 scaling:on origin:(0,0) degree:0"  "id:C8C68643-5FF2-456F-9994-0EF4246B77DB res:1920x1080 hz:75 color_depth:8 scaling:off origin:(992,-1080) degree:0" "id:479F2925-AEE2-42C8-B17A-CA96020D2C58  res:1920x1080 hz:75 color_depth:8 scaling:off origin:(-928,-1080) degree:0"
